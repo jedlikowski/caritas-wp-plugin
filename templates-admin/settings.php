82804306a16198b53ad5cfc680307533867b0be5
Jakub Jedlikowski <qba.jedlikowski@gmail.com>
2018-12-21 21:14:25 +0100

    dodanie instrukcji nadpisywania szablonów

--- templates-admin/settings.php
+++ templates-admin/settings.php
@@ -38,4 +38,19 @@
     </table>
     <?php submit_button();?>
   </form>
+  <h2>Nadpisywanie widoków</h2>
+  <p>
+      Wtyczka definiuje następujące linki oraz szablony odpowiadające za ich wyświetlanie:
+  </p>
+  <ul>
+      <li><code><?php echo site_url('/aktualnosci'); ?></code> - <code>news.php</code> (lista artykułów)</li>
+      <li><code><?php echo site_url('/aktualnosci/{id}'); ?></code> - <code>news-single.php</code> (szczegóły artykułu)</li>
+      <li><code><?php echo site_url('/cele'); ?></code> - <code>targets.php</code> (lista celów)</li>
+      <li><code><?php echo site_url('/cele/{id}'); ?></code> - <code>target-single.php</code> (szczegóły celu)</li>
+      <li><code><?php echo site_url('/cele/{id}/wesprzyj'); ?></code> - <code>target-payment-form.php</code> (formularz płatności)</li>
+      <li><code><?php echo site_url('/platnosc-zakonczona'); ?></code> - <code>payment-success.php</code> (sukces płatności)</li>
+      <li><code><?php echo site_url('/blad-platnosci'); ?></code> - <code>payment-success.php</code> (błąd płatności)</li>
+  </ul>
+  <p>Aby nadpisać szablon odpowiedzialny za wyświetlanie danego widoku należy w aktywnym motywie, w folderze <code>caritas-app-templates</code> utworzyć plik o tej samej nazwie jak szablon, który chcemy nadpisać.
+  <p>Pliki źródłowe znajdują się w folderze <code>templates</code> wewnątrz folderu zawierającego wtyczkę. Warto je wykorzystać jako punkt startowy przy tworzeniu własnych widoków.</p>
 </div><!-- .wrap -->
